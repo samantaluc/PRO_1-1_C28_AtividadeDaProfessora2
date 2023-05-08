df5bf3cc25c6e7310ee38abec35855149fa5878a
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-05-08 17:43:06 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,32 +3,28 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
 var engine, world, backgroundImg,
-waterSound,
-pirateLaughSound,
-backgroundMusic,
-cannonExplosion;
+waterSound,   //adicionar
+pirateLaughSound, //adicionar
+backgroundMusic,  //adicionar
+cannonExplosion;  //adicionar
 var canvas, angle, tower, ground, cannon, boat;
 var balls = [];
 var boats = [];
 var score = 0;
 var boatAnimation = [];
 var boatSpritedata, boatSpritesheet;
-
 var brokenBoatAnimation = [];
 var brokenBoatSpritedata, brokenBoatSpritesheet;
-
 var waterSplashAnimation = [];
 var waterSplashSpritedata, waterSplashSpritesheet;
-
 var isGameOver = false;
-var isLaughing= false;
-
+var isLaughing= false; //adicionar
 function preload() {
   backgroundImg = loadImage("./assets/background.gif");
-  backgroundMusic = loadSound("./assets/background_music.mp3");
-  waterSound = loadSound("./assets/cannon_water.mp3");
-  pirateLaughSound = loadSound("./assets/pirate_laugh.mp3");
-  cannonExplosion = loadSound("./assets/cannon_explosion.mp3");
+  backgroundMusic = loadSound("./assets/background_music.mp3"); //adicionar
+  waterSound = loadSound("./assets/cannon_water.mp3");  //adicionar
+  pirateLaughSound = loadSound("./assets/pirate_laugh.mp3");  //adicionar
+  cannonExplosion = loadSound("./assets/cannon_explosion.mp3"); //adicionar
   towerImage = loadImage("./assets/tower.png");
   boatSpritedata = loadJSON("assets/boat/boat.json");
   boatSpritesheet = loadImage("assets/boat/boat.png");
@@ -37,23 +33,17 @@ function preload() {
   waterSplashSpritedata = loadJSON("assets/water_splash/water_splash.json");
   waterSplashSpritesheet = loadImage("assets/water_splash/water_splash.png");
 }
-
 function setup() {
   canvas = createCanvas(1200,600);
   engine = Engine.create();
   world = engine.world;
   angleMode(DEGREES)
   angle = 15
-
-
   ground = Bodies.rectangle(0, height - 1, width * 2, 1, { isStatic: true });
   World.add(world, ground);
-
   tower = Bodies.rectangle(160, 350, 160, 310, { isStatic: true });
   World.add(world, tower);
-
   cannon = new Cannon(180, 110, 100, 50, angle);
-
   var boatFrames = boatSpritedata.frames;
   for (var i = 0; i < boatFrames.length; i++) {
     var pos = boatFrames[i].position;
@@ -79,38 +69,29 @@ function setup() {
 function draw() {
   background(189);
   image(backgroundImg, 0, 0, width, height);
-
-  if (!backgroundMusic.isPlaying()) {
-    backgroundMusic.play();
-    backgroundMusic.setVolume(0.1);
-  }
-
+  if (!backgroundMusic.isPlaying()) { //adicionar
+    backgroundMusic.play(); //adicionar
+    backgroundMusic.setVolume(0.1); //adicionar
+  } //adicionar
   Engine.update(engine);
- 
   push();
   translate(ground.position.x, ground.position.y);
   fill("brown");
   rectMode(CENTER);
   rect(0, 0, width * 2, 1);
   pop();
-
   push();
   translate(tower.position.x, tower.position.y);
   rotate(tower.angle);
   imageMode(CENTER);
   image(towerImage, 0, 0, 160, 310);
   pop();
-
   showBoats();
-
    for (var i = 0; i < balls.length; i++) {
     showCannonBalls(balls[i], i);
     collisionWithBoat(i);
   }
-
   cannon.display();
-  
-
   fill("#6d4c41");
   textSize(40);
   text(`Pontuação: ${score}`, width - 200, 50);
@@ -142,56 +123,41 @@ function keyPressed() {
     balls.push(cannonBall);
   }
 }
-
 function showCannonBalls(ball, index) {
   if (ball) {
     ball.display();
     ball.animate();
     if (ball.body.position.x >= width || ball.body.position.y >= height - 50) {
-      waterSound.play()  
-      ball.remove(index);
-      
+      waterSound.play();  //adicionar
+      ball.remove(index);     
     }
   }
 }
-
 function showBoats() {
   if (boats.length > 0) {
-    if (
-      boats.length < 4 &&
-      boats[boats.length - 1].body.position.x < width - 300
+    if (      boats.length < 4 &&      boats[boats.length - 1].body.position.x < width - 300
     ) {
       var positions = [-40, -60, -70, -20];
       var position = random(positions);
-      var boat = new Boat(
-        width,
-        height - 100,
-        170,
-        170,
-        position,
-        boatAnimation
+      var boat = new Boat(        width,        height - 100,        170,        170,        position,        boatAnimation
       );
-
       boats.push(boat);
     }
-
     for (var i = 0; i < boats.length; i++) {
       Matter.Body.setVelocity(boats[i].body, {
-        x: -0.9,
-        y: 0
+        x: -0.9,        y: 0
       });
-
       boats[i].display();
       boats[i].animate();
       var collision = Matter.SAT.collides(this.tower, boats[i].body);
       if (collision.collided && !boats[i].isBroken) {
           //Adicionar a sinalização isLaughing e a configuração isLaughing para true
-          if(!isLaughing && !pirateLaughSound.isPlaying()){
-            pirateLaughSound.play();
-            isLaughing = true
+          if(!isLaughing && !pirateLaughSound.isPlaying()){ //adicionar
+            pirateLaughSound.play(); //adicionar
+            isLaughing = true //adicionar
           }
         isGameOver = true;
-        gameOver();
+        gameOver(); //adicionar
       }
     }
   } else {
@@ -202,7 +168,7 @@ function showBoats() {
 
 function keyReleased() {
   if (keyCode === DOWN_ARROW && !isGameOver) {
-    cannonExplosion.play();
+    cannonExplosion.play(); //adicionar
     balls[balls.length - 1].shoot();
   }
 }
